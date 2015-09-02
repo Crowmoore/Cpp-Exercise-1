7c1b28e76252846d53e6d9d0e39551ea7b2f1099
Joel Kortelainen <jokke.kortelainen@gmail.com>
2015-09-02 19:21:25 +0300

    Uploaded the source file

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+int number = 1;
+int positive = 0;
+int negative = 0;
+int total = 0;
+
+int getUserNumber();
+void results();
+
+
+int main(void) {
+
+	while (number != 0) {
+		cout << "Insert an integer: ";
+		number = getUserNumber();
+		if (number > 0) {
+			positive++;
+		}
+		else if (number < 0) {
+			negative++;
+		}
+
+	}
+	results();
+	return(0);
+}
+
+void results() {
+
+	total = positive + negative;
+	int negativePros = (double)negative / (double)total * 100.0;
+	int positivePros = (double)positive / (double)total * 100.0;
+	cout << "You entered the following integers:" << endl;
+	cout << "Positive integers: " << positive << endl;
+	cout << "Positive number percentage: " << positivePros << "%" << endl;
+	cout << "Negative integers: " << negative << endl;
+	cout << "Negative number percentage: " << negativePros << "%" << endl;
+	cout << "Total integers: " << total << endl;
+	cout << "Total percentage: " << positivePros + negativePros << endl;
+}
+
+int getUserNumber() {
+
+	cin >> number;
+	return number;
+}
